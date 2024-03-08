bd4a686ed61a51727a7a713a66cc2742a101aafe
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-03-08 09:00:24 +0545

    if elseif else statements

--- index.php
+++ index.php
@@ -1,37 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/style.css" rel="stylesheet">
-</head>
-
-<body>
-    <form action="index.php" method="post">
-        <label for="radius">radius:</label><br>
-        <input type="text" name="radius"><br>
-        <input type="submit" value="calculate">
-    </form>
-</body>
-
-</html>
-
 <?php
-$radius = $_POST["radius"];
-$circumference = null;
-$area = null;
-$volume = null;
+$hours = 50;
+$rate = 15;
+$weekly_pay = null;
 
-$circumference = 2 * pi() * $radius;
-$circumference = round($circumference, 2);
+if ($hours <= 0) {
+    $weekly_pay = 0;
+} elseif ($hours <= 40) {
+    $weekly_pay = $hours * $rate;
+} else {
+    $weekly_pay = ($rate * 40) + (($hours - 40) * ($rate * 1.5));
+}
 
-$area = round(pi() * pow($radius, 2), 2);
-
-$volume = round(4 / 3 * pi() * pow($radius, 3), 2);
-
-echo "Circumference: {$circumference}<br>";
-echo "Area: {$area}<br>";
-echo "Volume: {$volume}<br>";
-?>
+echo "You earned \${$weekly_pay} this week";
