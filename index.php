1457e161a07926bcb88501ffe825a475ffea1080
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-03-09 16:41:10 +0545

    switches

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
 <?php
-$child = false;
-$senior = true;
-$ticket = null;
 
-if ($child || $senior) {
-    $ticket = 10;
-} else {
-    $ticket = 15;
+//switch - replacement to using many elseif statements
+// more efficient less code to writ
+
+$date = date("l");
+$date = "Friday";
+
+switch ($date) {
+    case "Sunday":
+        echo "I hate Sundays";
+        break;
+    case "Monday":
+        echo "Mon Mon day is today";
+        break;
+    case "Tuesday":
+        echo "Its Taco Tuesday";
+        break;
+    case "Wednesday":
+        echo "half day of the week";
+        break;
+    case "Thursday":
+        echo "Its almost the weekend";
+        break;
+    case "Friday":
+        echo "day before the weekend";
+        break;
+    case "Saturday":
+        echo "Finally the weekend";
+        break;
+    default:
+        echo "not a valid date";
 }
-echo "The ticket price is {$ticket}";
