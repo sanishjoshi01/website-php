459b28bc80d7acd8e2f492e23eaa489dd8cd1b6f
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-03-12 13:30:56 +0545

    radio buttons

--- index.php
+++ index.php
@@ -10,34 +10,37 @@
 
 <body>
     <form action="index.php" method="post">
-        <label for="username">Username: </label><br>
-        <input type="text" name="username"><br>
-        <label for="password">Password: </label><br>
-        <input type="password" name="password"><br>
-        <input type="submit" name="login" value="Log in"><br>
+        <input type="radio" name="credit_card" value="Visa">Visa<br>
+        <input type="radio" name="credit_card" value="Mastercard">Mastercard<br>
+        <input type="radio" name="credit_card" value="American Express">American Express<br>
+        <input type="submit" name="confirm" value="confirm"><br>
     </form>
 </body>
 
 </html>
 <?php
-//isset() - returns true if a variable is declared and not null
-//empty() - returns true if a variable is not declared, false, null, "" (empty)
 
-//behind the scenes of the post super global variable as it has key and value pairs.
-foreach ($_POST as $key => $value) {
-    echo "{$key} = {$value} <br>";
-}
+if (isset($_POST["confirm"])) {
+    $credit_card = null;
 
-if (isset($_POST["login"])) {
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    if (isset($_POST["credit_card"])) {
+        $credit_card = $_POST["credit_card"];
+    }
 
-    if (empty($username)) {
-        echo "Username is missing";
-    } elseif (empty($password)) {
-        echo "Password is missing";
-    } else {
-        echo "hello {$username}";
+
+    switch ($credit_card) {
+        case "Visa":
+            echo "You selected Visa";
+            break;
+        case "Mastercard";
+            echo "You selected Mastercard";
+            break;
+        case "American Express":
+            echo "You selected American Express";
+            break;
+        default:
+            echo "Please make a selection";
     }
 }
+
 ?>
