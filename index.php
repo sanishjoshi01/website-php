c4cac81e505e3a3419652a8fa67dbab6f2758345
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-03-13 11:46:45 +0545

    string functions

--- index.php
+++ index.php
@@ -1,16 +1,34 @@
 <?php
-function greetings($name, $age)
-{
-    echo "Hello {$name} <br>";
-    echo "You are {$age} years old <br>";
-}
 
-greetings("John", 23);
+$username = "John Doe";
+$phone = "123-456-7890";
 
-function hypotenuse($a, $b)
-{
-    $c = sqrt($a ** 2 + $b ** 2);
-    return $c;
-}
 
-echo hypotenuse(3, 4);
+// $username = strtolower($username);
+// $username = strtoupper($username);
+// $username = trim($username);
+
+// $username = str_pad($username, 10, 0);
+// John Doe00
+
+// $phone = str_replace("-", "", $phone);
+//1234567890
+
+// $username = strrev($username);
+// $username = str_shuffle($username);
+// $username = strcmp($username, "John Doe");
+
+// $username = strlen($username);
+// $username = strpos($username, " ");
+
+// $firstname = substr($username, 0, 4);
+// $lastname = substr($username, 4);
+// $fullname = explode(" ", $username);
+
+// foreach ($fullname as $name) {
+//     echo  $name . "<br>";
+// }
+$username2 = array("John", "Doe", "Great");
+$username2 = implode("-", $username2);
+
+echo $username2;
