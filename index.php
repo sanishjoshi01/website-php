1aa03685eeb8f160e1001374a8b583b17044c1ea
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-03-13 14:34:59 +0545

    cookies

--- index.php
+++ index.php
@@ -1,23 +1,10 @@
 <?php
-include("header.html");
-?>
 
-<!DOCTYPE html>
-<html lang="en">
+setcookie("fav_food", "burger", time() + (86500 * 2), "/");
+setcookie("fav_drinks", "sprite", time() + (86500 * 2), "/");
+setcookie("fav_dessert", "ice cream", time() + (86500 * 2), "/");
 
-<head>
-    <title></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/style.css" rel="stylesheet">
-</head>
 
-<body>
-    This is home page
-</body>
-
-</html>
-
-<?php
-include("footer.html")
-?>
+foreach ($_COOKIE as $key => $value) {
+    echo "{$key} = {$value} <br>";
+}
