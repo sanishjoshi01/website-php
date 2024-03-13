7cf0706e7959757362c24d1fd2b820c892625433
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-03-13 11:11:00 +0545

    functions

--- index.php
+++ index.php
@@ -1,32 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/style.css" rel="stylesheet">
-</head>
-
-<body>
-    <form action="index.php" method="post">
-        <input type="checkbox" name="foods[]" value="Pizza">Pizza<br>
-        <input type="checkbox" name="foods[]" value="Burger">Burger<br>
-        <input type="checkbox" name="foods[]" value="Hotdog">Hotdog<br>
-        <input type="checkbox" name="foods[]" value="Taco">Taco<br>
-        <input type="submit" name="submit"> <br>
-    </form>
-</body>
-
-</html>
 <?php
-
-if (isset($_POST["submit"])) {
-    $foods = $_POST["foods"];
-
-    foreach ($foods as $food) {
-        echo $food . "<br>";
-    }
+function greetings($name, $age)
+{
+    echo "Hello {$name} <br>";
+    echo "You are {$age} years old <br>";
 }
 
-?>
+greetings("John", 23);
+
+function hypotenuse($a, $b)
+{
+    $c = sqrt($a ** 2 + $b ** 2);
+    return $c;
+}
+
+echo hypotenuse(3, 4);
