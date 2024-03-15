f8f303f2172c4f898c27bb993a052fe1011bdc54
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-03-15 12:59:32 +0545

    hashing

--- index.php
+++ index.php
@@ -1,42 +1,13 @@
 <?php
-session_start();
 
-?>
 
-<!DOCTYPE html>
-<html lang="en">
+$password = "pizza123";
 
-<head>
-    <title>Login Page</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/style.css" rel="stylesheet">
-</head>
 
-<body>
-    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
-        username: <br>
-        <input type="text" name="username"><br>
-        password: <br>
-        <input type="password" name="password"><br>
-        <input type="submit" name="login" value="login">
-    </form>
-</body>
+$hash = password_hash($password, PASSWORD_DEFAULT);
 
-</html>
-
-<?php
-if (isset($_POST["login"])) {
-    if (
-        !empty($_POST["username"]) &&
-        !empty($_POST["password"])
-    ) {
-        $_SESSION["username"] = $_POST["username"];
-        $_SESSION["password"] = $_POST["password"];
-
-        header("Location: home.php");
-    } else {
-        echo "username/password missing";
-    }
+if (password_verify("pizza123", $hash)) {
+    echo "logged in";
+} else {
+    echo "incorrect password";
 }
-?>
