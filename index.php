f1d547422067d6bbf4b40eb06d3cfdb5bac7e651
sanishjoshi01 <sandeshjoshi2211@gmail.com>
2024-03-16 13:43:44 +0545

    registration form project

--- index.php
+++ index.php
@@ -1,16 +1,51 @@
 <?php
 include("database.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-$sql = "SELECT * FROM users";
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+</head>
 
-$result = mysqli_query($conn, $sql);
+<body>
+    <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        Welcome to Facebook (FAKE) <br>
+        username: <br>
+        <input type="text" name="username"><br>
+        password: <br>
+        <input type="password" name="password"><br>
+        <input type="submit" name="login" value="Register">
+    </form>
+</body>
 
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo $row["id"] . "<br>";
-        echo $row["username"] . "<br>";
-        echo $row["reg_date"] . "<br>";
+</html>
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    if (empty($username)) {
+        echo "Username is missing!";
+    } elseif (empty($password)) {
+        echo "Password is missing";
+    } else {
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+
+        $sql = "INSERT INTO users (username, password) VALUES ('$username','$hash')";
+
+        try {
+            mysqli_query($conn, $sql);
+            echo "'$username' is now registered. Thank you!!!";
+        } catch (mysqli_sql_exception) {
+            echo "'$username' is already taken";
+        }
     }
 }
 
 mysqli_close($conn);
+?>
